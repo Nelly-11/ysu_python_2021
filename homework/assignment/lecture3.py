6d83424c0fec5ecda2e208fc2a9c769268358ddc
Gevorg Ghalachyan <gevghalachyan@gmail.com>
2021-09-25 14:39:43 +0400

    added lecture 3 homework

---
+++ homework/assignment/lecture3.py
@@ -0,0 +1,48 @@
+"""
+This is the homework for the following topics
+    - data types
+    - control flows
+    - math, random modules
+Fill your from next line after # Answer:
+"""
+
+###################
+# Task 1:
+# Create and print 2 sets: ranks - ranks of cards; suits - suits of cards
+# Answer:
+
+###################
+
+###################
+# Task 2:
+# Creat and print a list of tuples: deck - deck of 52 cards, using ranks and suits sets:
+# [("2", "spades"), ("3", "spades"),...]
+# Hint: use nested loops
+# Answer:
+
+###################
+###################
+# Task 3:
+# Print the length of deck
+# Print if it is equal to 52
+# Answer:
+
+###################
+###################
+# Task 4:
+# Print top card of the deck
+# Shuffle the deck of of cards
+# Print top card of the deck
+# Hint: use random module
+# Answer:
+
+###################
+###################
+# Task 5:
+# Create hand_values dictionary of ranks to soft hand and hard hand value: {rank: (soft_hand, hard_hand)}
+# Print the result
+# Example: {"ace": (1,11), "2": (2,2),...}
+# Hint: use this logic - for ace if is (1,11) for number it is the the face value for the rest it is 10
+# Answer:
+
+###################
